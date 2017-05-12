8372304500e743a037fc4857d1bd579c19daec9a
OloinMilsom <keporagebora@gmail.com>
2017-05-12 16:13:23 +0100

    initial

---
+++ Game1/Actors.fs
@@ -0,0 +1,32 @@
+﻿module Actors
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+open Microsoft.Xna.Framework.Content
+
+type BodyType = 
+    | Static
+    | Dynamic of Vector2
+
+type PlayerState =
+    | Nothing
+    | Jumping
+ 
+type ActorType =
+    | Player of PlayerState
+    | Obstacle
+ 
+type WorldActor =
+        ActorType *
+        Vector2 *
+        Vector2 *
+        Texture2D option *
+        BodyType
+
+let getBounds ((_, p, s, _, _):WorldActor) : Rectangle =
+    Rectangle(int p.X, int p.Y, int s.X, int s.Y)
+
+let getDesiredBounds ((_, p, s, _, b):WorldActor) : Vector2 =
+    match b with
+    | Dynamic(s) -> p + s
+    | _ -> p
---
+++ Game1/Actors.fsi
@@ -0,0 +1,28 @@
+﻿module Actors
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+open Microsoft.Xna.Framework.Content
+
+type BodyType = 
+    | Static
+    | Dynamic of Vector2
+
+type PlayerState =
+    | Nothing
+    | Jumping
+ 
+type ActorType =
+    | Player of PlayerState
+    | Obstacle
+ 
+type WorldActor =
+        ActorType *
+        Vector2 *
+        Vector2 *
+        Texture2D option *
+        BodyType
+
+val getBounds : WorldActor -> Rectangle 
+
+val getDesiredBounds : WorldActor -> Vector2
